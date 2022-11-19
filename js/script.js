fdcb8877efb61679485904430bd002b7035129c9
Bum Bam <timurrozmetow17@gmail.com>
2022-11-19 23:15:30 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const Prosent = 12;
+let Potratil = 0,
+    Bank = 500,
+    dynya=42,
+    grusha=39,
+    n=1;
+
+let sum =grusha+dynya;
+
+while(Potratil<=Bank){
+    Potratil=+(dynya%Prosent)*dynya;
+    if(Potratil+=grusha<Bank){
+        Potratil=+(grusha%Prosent)*grusha;
+    }
+    console.log(n++,'Amount', Potratil, "Ostatok v banke", Bank-=Potratil);
+
+}
